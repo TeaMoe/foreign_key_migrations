b3eb717063ef61ff2338055e75584d143fac59d4
Timo Göllner <timo.goellner@gmail.com>
2009-12-22 11:01:28 +0100

    renamed libs

--- init.rb
+++ init.rb
@@ -1,2 +1,2 @@
-require 'core' unless defined?(Core)
-require 'foreign_key_migrations' unless defined?(ForeignKeyMigrations)
+require 'core_lib' unless defined?(Core)
+require 'foreign_key_migrations_lib' unless defined?(ForeignKeyMigrations)
